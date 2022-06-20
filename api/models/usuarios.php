f5f403b24caf52c752b21f2cd5f88799d3c4f7ab
EyleenSantos <85887967+EyleenSantos@users.noreply.github.com>
2022-06-19 21:02:38 -0600

    trabajito honesto

---
+++ api/models/usuarios.php
@@ -0,0 +1,293 @@
+<?php
+/*
+*	Esta sera la clase para manejar la tabla usuarios de la base de datos.
+*   Es una clase hija de Validator.
+*/
+class Usuarios extends Validator
+{
+    // Declaración de atributos (propiedades) según nuestra tabla en la base de datos.
+    private $id = null;
+    private $nombres = null;
+    private $apellidos = null;
+    private $cargo = null;
+    private $telefono = null;
+    private $foto = null;
+    private $estado = null;
+    private $correo = null;
+    private $alias = null;
+    private $clave = null;
+
+    /*
+    *   Métodos para validar y asignar valores de los atributos.
+    */
+    public function setId($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->id = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setNombres($value)
+    {
+        if ($this->validateAlphabetic($value, 1, 50)) {
+            $this->nombres = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setApellidos($value)
+    {
+        if ($this->validateAlphabetic($value, 1, 50)) {
+            $this->apellidos = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setCargo($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->cargo = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    public function setTelefono($value)
+    {   
+        if ($this->validatePhone($value)) {
+            $this->telefono = $value;
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+
+    public function setCorreo($value)
+    {
+        if ($this->validateEmail($value)) {
+            $this->correo = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setAlias($value)
+    {
+        if ($this->validateAlphanumeric($value, 1, 50)) {
+            $this->alias = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setClave($value)
+    {
+        if ($this->validatePassword($value)) {
+            $this->clave = password_hash($value, PASSWORD_DEFAULT);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function setFoto($value)
+    {
+        $this->foto = $value;
+        return true;
+    }
+
+    public function setEstado($value)
+    {
+        if ($this->validateNaturalNumber($value)) {
+            $this->estado = $value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    /*
+    *   Métodos para obtener valores de los atributos.
+    */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNombres()
+    {
+        return $this->nombres;
+    }
+
+    public function getApellidos()
+    {
+        return $this->apellidos;
+    }
+
+    public function getCargo()
+    {
+        return $this->cargo;
+    }
+
+    public function getTelefono()
+    {
+        return $this->telefono;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function getCorreo()
+    {
+        return $this->correo;
+    }
+
+    public function getAlias()
+    {
+        return $this->alias;
+    }
+
+    public function getClave()
+    {
+        return $this->clave;
+    }
+    
+    public function getFoto()
+    {
+        return $this->foto;
+    }
+
+    /*
+    *   Métodos para gestionar la cuenta de la tabla usuario.
+    */
+
+    // Se verifica si hay concidencias de información mediante el correo del empleado ingresado-------------------------.
+
+    public function checkUser($correo)
+    {
+        $sql = 'SELECT "idEmpleado", "estadoEmpleado" FROM empleado WHERE "correoEmpleado" = ? or "aliasEmpleado" = ?';
+        $params = array($correo, $correo);
+        if ($data = Database::getRow($sql, $params)) {
+            $this->id = $data['idEmpleado'];
+            $this->estado = $data['estadoEmpleado'];
+            $this->correo = $correo;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function checkPassword($password)
+    {
+        $sql = 'SELECT "contrasenaEmpleado" FROM empleado WHERE "idEmpleado" = ?';
+        $params = array($this->id);
+        $data = Database::getRow($sql, $params);
+        // Se verifica si la contraseña coincide con el hash almacenado en la base de datos.
+        if (password_verify($password, $data['contrasenaEmpleado'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function changePassword()
+    {
+        $sql = 'UPDATE empleado SET claveEmpleado = ? WHERE idEmpleado = ?';
+        $params = array($this->clave, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+    /* 
+    *   Método para comprobar que existen usuarios registrados en nuestra base de datos
+    */
+
+    // Método para leer toda la información de los usuarios registrados-------------------------.
+    public function readAll()
+    {
+        $sql = 'SELECT "idEmpleado", "nombresEmpleado", "apellidosEmpleado", ce."cargoEmpleado", ee."estadoEmpleado"
+        FROM empleado as e inner join "cargoEmpleado" as ce on e."cargoEmpleado" = ce."idCargoEmpleado"
+        inner join "estadoEmpleado" as ee on e."estadoEmpleado" = ee."idEstadoEmpleado" 
+		order by "idEmpleado", e."estadoEmpleado"';
+        
+        $params = null;
+        return Database::getRows($sql, $params);
+    }
+
+    // Método para un dato en especifico de los usuarios registrados-------------------------.
+    public function readOne()
+    {
+        $sql = 'SELECT "idEmpleado", "nombresEmpleado", "apellidosEmpleado", "correoEmpleado", "aliasEmpleado", "fotoEmpleado", "cargoEmpleado", "estadoEmpleado"
+        FROM empleado
+        where "idEmpleado" = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    /* Método para obtener un empleado y mostrarlo en modal de visualizar*/
+    public function readOneShow()
+    {
+        $sql = 'SELECT "idEmpleado", "nombresEmpleado", "apellidosEmpleado", "correoEmpleado",  "aliasEmpleado", a.avatar, ce."cargoEmpleado", ee."estadoEmpleado"
+        FROM empleado as e inner join "cargoEmpleado" as ce on e."cargoEmpleado" = ce."idCargoEmpleado"
+        inner join "estadoEmpleado" as ee on e."estadoEmpleado" = ee."idEstadoEmpleado"
+		inner join "avatar" as a on e."fotoEmpleado" = a."idAvatar" 
+        where "idEmpleado" = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+    /*
+    *   Métodos para realizar las operaciones SCRUD (search, create, read, update, delete).
+    */
+    /* SEARCH */
+    public function searchRows($value)
+    {
+        $sql = 'SELECT "idEmpleado", "nombresEmpleado", "apellidosEmpleado", ce."cargoEmpleado", ee."estadoEmpleado"
+                FROM empleado as e inner join "cargoEmpleado" as ce on e."cargoEmpleado" = ce."idCargoEmpleado"
+                inner join "estadoEmpleado" as ee on e."estadoEmpleado" = ee."idEstadoEmpleado"
+                WHERE "nombresEmpleado" ILIKE ? OR "apellidosEmpleado" ILIKE ?
+                order by "idEmpleado", e."estadoEmpleado"';
+        $params = array("%$value%", "%$value%");
+        return Database::getRows($sql, $params);
+    }
+
+    /* CREATE */
+    public function createRow()
+    {
+        $sql = 'INSERT INTO empleado("nombresEmpleado", "apellidosEmpleado", "correoEmpleado", "aliasEmpleado", "contrasenaEmpleado", "direccionEmpleado", "telefonoEmpleado", "fotoEmpleado", "cargoEmpleado", "estadoEmpleado")
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);';
+        $params = array($this->nombres, $this->apellidos, $this->correo, $this->alias, $this->clave, $this->direccion, $this->telefono, $this->foto, $this->cargo, $this->estado);
+        return Database::executeRow($sql, $params);
+    }
+
+
+    /* UPDATE */
+    public function updateRow()
+    {
+        $sql = 'UPDATE empleado
+                SET "nombresEmpleado" = ?, "apellidosEmpleado" = ?, "correoEmpleado" = ?, "direccionEmpleado" = ?, "telefonoEmpleado" = ?, "cargoEmpleado" = ?, "estadoEmpleado" = ?, "fotoEmpleado" = ?
+                WHERE "idEmpleado" = ?';
+            $params = array($this->nombres, $this->apellidos, $this->correo, $this->direccion, $this->telefono, $this->cargo, $this->estado, $this->foto, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    /* DELETE */
+    /* Función para inhabilitar un usuario ya que no los borraremos de la base------------------------- */
+    public function deleteRow()
+    {
+        //No eliminaremos registros, solo los inhabilitaremos-------------------------.
+        $sql = 'UPDATE empleado SET "estadoEmpleado" = 3 WHERE "idEmpleado" = ?'; //Delete from empleado where "idEmpleado" = ? -------------------------.
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+}
---
+++ api/private/usuarios.php
@@ -0,0 +1,227 @@
+<?php
+require_once('../helpers/database.php');
+require_once('../helpers/validaciones.php');
+require_once('../models/usuarios.php');
+
+// Se comprueba si existe una acción a realizar por medio de isset, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $usuario = new Usuarios;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'session' => 0, 'message' => null, 'exception' => null, 'dataset' => null, 'username' => null, 'avatar' => null);
+    // Se verifica si existe una sesión iniciada como administrador, de lo contrario se finaliza el script con un mensaje de error.
+    if (isset($_SESSION['id_usuario'])) {
+        $result['session'] = 1;
+        // Se compara la acción a realizar cuando un administrador ha iniciado sesión.
+        switch ($_GET['action']) {
+            // Accion de leer la información en base al Correo ------------------.
+            case 'getUser':
+                if (isset($_SESSION['correoEmpleado'])) {
+                    $result['status'] = 1;
+                    $result['username'] = $_SESSION['correoEmpleado'];
+                    $result['avatar'] = $_SESSION['avatar'];
+                } else {
+                    $result['exception'] = 'Correo de usuario indefinido';
+                }
+                break;
+            // Accion de cerrar  secion------------------.        
+            case 'logOut':
+                if (session_destroy()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Sesión eliminada correctamente';
+                } else {
+                    $result['exception'] = 'Ocurrió un problema al cerrar la sesión';
+                }
+                break;
+            // Accion de leer toda la información------------------.    
+            case 'readAll':
+                if ($result['dataset'] = $usuario->readAll()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'No hay datos registrados';
+                }
+                break;
+            // Accion de buscar información de los usuarios disponibles------------------.     
+            case 'search':
+                if ($result['dataset'] = $usuario->searchRows($_POST['search'])) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                }else {
+                    $result['exception'] = 'No hay coincidencias';
+                }
+                break;
+            // Accion de crear un nuevo usuario ------------------.       
+            case 'create':
+                //Especificamos los inputs por medio de su atributo name, y los capturamos con el método post
+                $_POST = $usuario->validateForm($_POST);
+                if (!$usuario->setNombres($_POST['nombres'])) {
+                    $result['exception'] = 'Nombres inválidos';
+                } elseif (!$usuario->setApellidos($_POST['apellidos'])) {
+                    $result['exception'] = 'Apellidos inválidos';
+                } elseif (!$usuario->setCargo($_POST['cargo'])){
+                    $result['exception'] = 'Cargo inválido';
+                } elseif (!$usuario->setCorreo($_POST['correo'])) {
+                    $result['exception'] = 'Correo inválido';
+                } elseif (!$usuario->setDireccion($_POST['direccion'])) {
+                    $result['exception'] = 'Direccion inválida';
+                } elseif (!$usuario->setTelefono($_POST['telefono'])) {
+                    $result['exception'] = 'Teléfono inválido';
+                }elseif (!$usuario->setFoto($_POST['foto'])) {
+                    $result['exception'] = 'Foto inválida';
+                }elseif (!$usuario->setEstado(1)) {
+                    $result['exception'] = 'Estado inválido';
+                }elseif (!$usuario->setAlias($_POST['alias'])) {
+                    $result['exception'] = 'Alias inválido';
+                } elseif ($_POST['clave'] != $_POST['confirmar']) {
+                    $result['exception'] = 'Claves diferentes';
+                } elseif (!$usuario->setClave($_POST['clave'])) {
+                    $result['exception'] = $usuario->getPasswordError();
+                } elseif ($usuario->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Empleado creado correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            // Accion leer un elemento de toda la información------------------.       
+            case 'readOne':
+                if (!$usuario->setId($_POST['id'])) {
+                    $result['exception'] = 'Empleado incorrecto';
+                } elseif ($result['dataset'] = $usuario->readOne()) {
+                    $result['status'] = 1;
+                } elseif (Database::getException()) {
+                    $result['exception'] = Database::getException();
+                } else {
+                    $result['exception'] = 'Empleado inexistente';
+                }
+                break;
+            // Accion de leer si el id existe del empleado------------------.     
+            case 'readOneShow':
+                    if (!$usuario->setId($_POST['id'])) {
+                        $result['exception'] = 'Empleado incorrecto';
+                    } elseif ($result['dataset'] = $usuario->readOneShow()) {
+                        $result['status'] = 1;
+                    } elseif (Database::getException()) {
+                        $result['exception'] = Database::getException();
+                    } else {
+                        $result['exception'] = 'Empleado inexistente';
+                    }
+                    break;
+            // Accion de actualizar un elemento de toda la información------------------.         
+            case 'update':
+                //Especificamos los inputs por medio de su atributo name, y los capturamos con el método post
+                $_POST = $usuario->validateForm($_POST);
+                if (!$usuario->setId($_POST['id'])) {
+                    $result['exception'] = 'Empleado incorrecto';
+                } elseif (!$usuario->readOne()) {
+                    $result['exception'] = 'Empleado inexistente';
+                } if (!$usuario->setNombres($_POST['nombres'])) {
+                    $result['exception'] = 'Nombres inválidos';
+                } elseif (!$usuario->setApellidos($_POST['apellidos'])) {
+                    $result['exception'] = 'Apellidos inválidos';
+                } elseif (!$usuario->setCargo($_POST['cargo'])){
+                    $result['exception'] = 'Cargo inválido';
+                } elseif (!$usuario->setCorreo($_POST['correo'])) {
+                    $result['exception'] = 'Correo inválido';
+                } elseif (!$usuario->setDireccion($_POST['direccion'])) {
+                    $result['exception'] = 'Direccion inválida';
+                } elseif (!$usuario->setTelefono($_POST['telefono'])) {
+                    $result['exception'] = 'Teléfono inválido';
+                }elseif (!$usuario->setFoto($_POST['foto'])) {
+                    $result['exception'] = 'Foto inválida';
+                }elseif (!$usuario->setEstado($_POST['estado'])) {
+                    $result['exception'] = 'Estado inválido';
+                }elseif ($usuario->updateRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Empleado modificado correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+            // Accion de desabilitar un elemento de toda la información------------------.        
+            case 'delete':
+                if ($_POST['idEmpleado'] == $_SESSION['id_usuario']) {
+                    $result['exception'] = 'No se puede dar de baja a sí mismo';
+                } elseif (!$usuario->setId($_POST['idEmpleado'])) {
+                    $result['exception'] = 'Empleado incorrecto';
+                } elseif (!$usuario->readOne()) {
+                    $result['exception'] = 'Empleado inexistente';
+                } elseif ($usuario->deleteRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Empleado inhabilitado correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;    
+            default:
+                $result['exception'] = 'Acción no disponible dentro de la sesión';
+        }
+    } else {
+        // Se compara la acción a realizar cuando el administrador no ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'readUsers':
+                //Verificamos si existen usuarios registrados en la base de datos, para que en caso de no, registrar el primer usuario
+                if ($usuario->readAll()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Existe al menos un usuario registrado';
+                } else {
+                    $result['exception'] = 'No existen usuarios registrados';
+                }
+                break;
+            case 'register-first-user':
+                //Especificamos los inputs por medio de su atributo name, y los capturamos con el método post
+                $_POST = $usuario->validateForm($_POST);
+                if (!$usuario->setNombres($_POST['nombres'])) {
+                    $result['exception'] = 'Nombres inválidos';
+                } elseif (!$usuario->setApellidos($_POST['apellidos'])) {
+                    $result['exception'] = 'Apellidos inválidos';
+                } elseif (!$usuario->setCargo(1)){
+                    $result['exception'] = 'Cargo inválido';
+                } elseif (!$usuario->setCorreo($_POST['correo'])) {
+                    $result['exception'] = 'Correo inválido';
+                }elseif (!$usuario->setEstado(1)) {
+                    $result['exception'] = 'Estado inválido';
+                }elseif (!$usuario->setAlias($_POST['alias'])) {
+                    $result['exception'] = 'Alias inválido';
+                } elseif ($_POST['clave'] != $_POST['confirmar']) {
+                    $result['exception'] = 'Claves diferentes';
+                } elseif (!$usuario->setClave($_POST['clave'])) {
+                    $result['exception'] = $usuario->getPasswordError();
+                } elseif ($usuario->createRow()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Usuario registrado correctamente';
+                } else {
+                    $result['exception'] = Database::getException();
+                }
+                break;
+                case 'logIn':
+                    $_POST = $usuario->validateForm($_POST);
+                    if (!$usuario->checkUser($_POST['correo'])) {
+                        $result['exception'] = 'Correo incorrecto';
+                    } elseif (!$usuario->getEstado()) {
+                        $result['exception'] = 'La cuenta ha sido desactivada';
+                    } elseif ($usuario->checkPassword($_POST['clave'])) {
+                        $result['status'] = 1;
+                        $result['message'] = 'Autenticación correcta';
+                        $_SESSION['idEmpleado'] = $usuario->getId();
+                        $_SESSION['correoEmpleado'] = $usuario->getCorreo();
+                    } else {
+                        $result['exception'] = 'Clave incorrecta';
+                    }
+                    break;
+            default:
+                $result['exception'] = 'Acción no disponible fuera de la sesión';
+        }
+    }
+    // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+    header('content-type: application/json; charset=utf-8');
+    // Se imprime el resultado en formato JSON y se retorna al controlador.
+    print(json_encode($result));
+} else {
+    print(json_encode('Recurso no disponible'));
+}
