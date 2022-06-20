53202e4b5c1819aad8df66ab5a6b799b8308997f
jefryvs <jefry.panameno02@gmail.com>
2022-06-20 13:25:13 -0600

    avances productos

--- controllers/private/productos.js
+++ controllers/private/productos.js
@@ -1,6 +1,12 @@
-/*Inicializando y configurando tabla*/
-$(document).ready(function () {
-    $('#example').DataTable({
+const API_PRODUCTOS = SERVER + 'private/productos.php?action=';
+
+// Método manejador de eventos que se ejecuta cuando el documento ha cargado.
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    readRows(API_PRODUCTOS);
+    /*Inicializando y configurando tabla*/
+    let options = {
         "info": false,
         "columnDefs": [
             {
@@ -34,9 +40,12 @@ $(document).ready(function () {
             }
         },
         "lengthMenu": [[10, 15, 20, -1], [10, 15, 20, "Todos"]]
-    });
+    };
+    let table = new DataTable('#example', options);
+
     /*Función para mostrar y ocultar campos de la tabla*/
-    $('#checkTabla').change(function () {
+    document.getElementById('checkTabla').addEventListener('change', function () {
         $('#example').DataTable().columns([4, 5, 6, 7]).visible($(this).is(':checked'))
-    })
+    });
 });
+
